afddf14a92111e7625e53b2c3c7ccac1417a7f50
Jon Cockle <yongfook@gmail.com>
2019-09-12 09:12:53 +0800

    add cache

--- app/controllers/api/v1/chartmogul_controller.rb
+++ app/controllers/api/v1/chartmogul_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::ChartmogulController < ApplicationController
 	def metrics
-		auth = {:username => ENV['CHARTMOGUL_TOKEN'], :password => ENV['CHARTMOGUL_KEY']}
-		today = Time.now.strftime("%Y-%m-%d")
-		@metrics = HTTParty.get("https://api.chartmogul.com/v1/metrics/all?start-date=2019-09-11&end-date=#{today}", :basic_auth => auth)
-		render :json => @metrics
+		data = Rails.cache.fetch('api:open:chartmogul', {expires_in: 5.minutes, public: true, raw: true}) do
+			auth = {:username => ENV['CHARTMOGUL_TOKEN'], :password => ENV['CHARTMOGUL_KEY']}
+			today = Time.now.strftime("%Y-%m-%d")
+			return HTTParty.get("https://api.chartmogul.com/v1/metrics/all?start-date=2019-09-11&end-date=#{today}", :basic_auth => auth)
+		end
+		render :json => @data
 	end
 end
