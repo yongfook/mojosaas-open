c903442f8947ee13f0cf480c973209535eec488a
Jon Cockle <yongfook@gmail.com>
2019-09-12 09:27:55 +0800

    console output

--- app/controllers/api/v1/chartmogul_controller.rb
+++ app/controllers/api/v1/chartmogul_controller.rb
@@ -6,6 +6,7 @@ class Api::V1::ChartmogulController < ApplicationController
 
 	def metrics
 		@data = Rails.cache.fetch('api:open:chartmogul', {expires_in: 5.minutes, public: true, raw: true}) do
+			puts "Fetching fresh data"
 			auth = {:username => ENV['CHARTMOGUL_TOKEN'], :password => ENV['CHARTMOGUL_KEY']}
 			today = Time.now.strftime("%Y-%m-%d")
 			HTTParty.get("https://api.chartmogul.com/v1/metrics/all?start-date=2019-09-11&end-date=#{today}", :basic_auth => auth)
