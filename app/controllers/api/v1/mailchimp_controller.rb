045698c207427f2afb22cb06deac802c80dc5ae8
Jon Cockle <yongfook@gmail.com>
2019-09-16 14:14:48 +0800

    ok clearly i dont understand rails caching as well as i thought

--- app/controllers/api/v1/mailchimp_controller.rb
+++ app/controllers/api/v1/mailchimp_controller.rb
@@ -5,7 +5,8 @@ class Api::V1::MailchimpController < ApplicationController
 		@data = Rails.cache.fetch('api:open:mailchimp', {expires_in: 5.minutes, public: true, raw: true}) do
 			puts "Fetching fresh data"
 			auth = {:username => "mailchimp docs say any string is ok here", :password => ENV['MAILCHIMP_API_KEY']}
-			HTTParty.get("#{mailchimp_root}/lists/#{ENV['MAILCHIMP_LIST_ID']}", :basic_auth => auth)['stats']
+			response = HTTParty.get("#{mailchimp_root}/lists/#{ENV['MAILCHIMP_LIST_ID']}", :basic_auth => auth)
+			response['stats']
 		end
 		render :json => @data
 	end
