d217f41dcbfeb683872504e2fe46c5c09e0a3d36
jro <jeroen@roosenboom.com>
2019-10-12 14:25:32 +0200

    Move CORS to initializers

--- config/application.rb
+++ config/application.rb
@@ -33,12 +33,5 @@ module MojosaasOpen
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get]
-      end
-    end
   end
 end
